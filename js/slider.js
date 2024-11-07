a0455015b12c5bf6e6fa99541b92717e22bc98fd
João Siqueira <joaovictorsanto800@gmail.com>
2024-11-07 13:26:53 -0300

    Adicionando animação js nas imagens

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,44 @@
-document.addEventListener('DOMContentLoaded', function() {
+let time = 2000; //determina um inntervalo de tempo  de 2s
+let currentImageIndex = 0; //o contador inicia no indice 0 (imagem 1)
+let images = document.querySelectorAll(".sobre-icones img"); //importando imagens
+let max = images.length; //determina o máximo de imagens no indice
+
+
+function nextImage() {
+
+    images[currentImageIndex].classList.remove("selected") //remove a class 'selected' para não exibir imagem
+
+
+    currentImageIndex++ //incrementa mais um no contador
+
+    if (currentImageIndex >= max) { // se o contador for maior que o numero de imagens ele retorna a 0
+        currentImageIndex = 0
+    }
+
+    images[currentImageIndex].classList.add("selected") //adiciona a classe 'selected' para exibir imagem
+}
+
+function start() { //Repete a função com intervalo  de 2s para cada imagem
+    setInterval(() => {
+        //troca de imagens
+        nextImage()
+    }, time)
+}
+
+
+window.addEventListener("load", start) //inicia a ação quano a janela é carregada
+
+
+
+
+
+
+
+
+
+
+
+/*document.addEventListener('DOMContentLoaded', function() {
     const slider = document.querySelector('.sobre-icones');
     const images = document.querySelectorAll('.sobre-icones span');
     const imageWidth = 150; // largura de uma imagem
@@ -26,4 +66,4 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Define o intervalo para trocar a imagem a cada 2 segundos (2000 ms)
     setInterval(showNextImage, 2000);
-});
+});*/
